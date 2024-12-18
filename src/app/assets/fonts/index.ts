c1e1b5bc299c0a25d4e5dbce70c0b452805c7424
HenriqueSenaDev <acopsenadeveloper@gmail.com1>
2024-12-18 12:12:52 -0300

    feat: project clean-up and font-assets  config

---
+++ src/app/assets/fonts/index.ts
@@ -0,0 +1,11 @@
+import { Outfit, DM_Sans } from 'next/font/google'
+
+export const OutfitFontFamily = Outfit({
+    display: "swap",
+    subsets: ["latin"],
+});
+
+export const DM_SansFontFamily = DM_Sans({
+    display: "swap",
+    subsets: ["latin"],
+});
