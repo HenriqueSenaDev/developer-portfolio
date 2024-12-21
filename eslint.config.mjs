ff9f3499083527081534d4314d868b042d92b66e
HenriqueSenaDev <acopsenadeveloper@gmail.com1>
2024-12-21 20:38:23 -0300

    feat/style-guides: .editorconfig, eslint and prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -10,7 +13,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  js.configs.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintPluginPrettierRecommended, // must be last item
 ];
 
 export default eslintConfig;
