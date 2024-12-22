483f6c8583497e25495c04d69da0d368943b3575
HenriqueSenaDev <acopsenadeveloper@gmail.com1>
2024-12-22 07:19:56 -0300

    feat/design-system: select component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { OutfitFontFamily } from "./assets/fonts";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
