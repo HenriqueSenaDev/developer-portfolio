a70521eeb5ab788c13fc2876faa1642500c2ac0c
HenriqueSenaDev <acopsenadeveloper@gmail.com1>
2024-12-22 16:22:07 -0300

    feat: home page mobile hero section

---
+++ src/app/(home)/hero/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Image from "next/image";
+import heroImgSrc from "@/assets/images/hero-personal.png";
+import TextLoop from "react-text-loop";
+import { DMSansFontFamily } from "@/assets/fonts";
+import { Button } from "@/components/button";
+import { MessageSquareIcon, PickaxeIcon } from "lucide-react";
+
+export const Hero = () => {
+  return (
+    <section className="flex flex-col px-5 mt-[22px]">
+      <div className="flex flex-col gap-7">
+        <div className="flex flex-col gap-3">
+          <div
+            className={`${DMSansFontFamily.className} font-bold leading-tight`}
+          >
+            <h1 className="text-[26px]">
+              Desenvolva com{" "}
+              <TextLoop className="text-primary" interval={3000}>
+                <strong>eficiência</strong>
+                <strong>agilidade</strong>
+                <strong>confiança</strong>
+              </TextLoop>
+            </h1>
+
+            <h3 className="text-xl">
+              software{" "}
+              <TextLoop className="text-primary" interval={5000}>
+                <strong>de grande valor.</strong>
+                <strong>de qualidade.</strong>
+                <strong>performático.</strong>
+              </TextLoop>
+            </h3>
+          </div>
+
+          <p className="text-sm font-light">
+            Desenvolvedor Full-Stack capacitado e independente. Aprimore seu
+            negócio com as funcionalidades mais modernas e sofisticadas do
+            mercado.
+          </p>
+        </div>
+
+        <div className="flex flex-wrap gap-x-3 gap-y-2">
+          <Button className="h-9 px-3 gap-[6px] text-xs">
+            <PickaxeIcon />O que posso construir
+          </Button>
+
+          <Button variant="secondary" className="h-9 px-3 gap-[6px] text-xs">
+            <MessageSquareIcon />
+            Entre em contato
+          </Button>
+        </div>
+      </div>
+
+      <Image
+        className="w-full max-w-[232px] h-auto mx-auto mt-8"
+        // ! temporary alternative description
+        alt="temporary alternative"
+        src={heroImgSrc}
+      />
+    </section>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./header";
+import { Hero } from "./hero";
 
 export default function Home() {
   return (
     <div className="h-screen">
       <Header />
+      <Hero />
     </div>
   );
 }
