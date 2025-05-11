4a92853a1f458644bf67dd81446f159ef5223413
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2025-05-11 09:27:32 -0300

    fix: locales type.

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,9 @@
+import { Locales } from "@/i18n/locale";
 import { Messages } from "@/i18n/types/messages";
 
-declare module 'next-intl' {
+declare module "next-intl" {
   interface AppConfig {
+    Locale: Locales;
     Messages: Messages;
   }
 }
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,13 +3,14 @@
 import { LOCALE_COOKIE_KEY } from "@/config/cookies";
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
+import { Locales } from "./locale";
 
 // Server action to provide i18n metadata for server-components.
 export default getRequestConfig(async () => {
   const reqCookies = await cookies();
   const localeCookie = reqCookies.get(LOCALE_COOKIE_KEY);
 
-  const locale = localeCookie ? localeCookie.value : "pt-BR";
+  const locale = (localeCookie ? localeCookie.value : "pt-BR") as Locales;
 
   return {
     locale,
